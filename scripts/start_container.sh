fe8b7236beca443071949fea7f1b0907e728d9fd
Lalatendu Jena <lalatendujena007@gmail.com>
2024-02-18 12:37:00 +0530

    new port

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull lalatendu007/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 lalatendu007/simple-python-flask-app
+docker run -d -p 5001:5001 lalatendu007/simple-python-flask-app
